81178b1f837043470a5ae9edd0743a80bcc19039
Гуськов Андрей Игоревич <C:\Users\GuskovAI\AppData\Roaming\The Bat!>
2017-10-13 16:51:39 +0300

    Home work 2017.10.13_1

--- JavaScript-основы/06-Массивы/practice/06.08.js
+++ JavaScript-основы/06-Массивы/practice/06.08.js
@@ -1,10 +1,23 @@
-let s=14,
-    arr=[2,1,2,4,3,5,2,6];
-for (var i in arr-1){
-  var tmpS=S-arr[i];
-    if (tmpS > 0) {
-        for (let ln=arr.length,j=i+1; j<ln; j++){
-            
-        }
+let s=14, ss,
+    inArr=[2,1,2,4,3,5,2,6];
+//        inArr=[2,1,2,4,5,5,2,6];
+function summ (arr){
+    sum=0;
+    for (let i in arr){
+        sum+=arr[i];
+    }
+    return sum;
+}
+let sumArr=[];
+for (let i in inArr){
+    sumArr.push(inArr[i]);
+    ss=summ(sumArr);
+    console.log(i, inArr[i], ss);
+    if ( ss > s ) {
+        sumArr.splice((sumArr.length -1),1);
+    } else if (ss == s ){
+        break;
     }
 }
+if ( ss != s ) sumArr =-1;
+console.log(sumArr);
--- JavaScript-основы/08-Объекты/practice/08.06.js
+++ JavaScript-основы/08-Объекты/practice/08.06.js
@@ -6,3 +6,14 @@ var people = [
   { firstname: 'Asdf', lastname: 'Xyz', age: 81 },
   { firstname: 'Gosho', lastname: 'Gosho', age: 22 }
 ];
+function group (inArr){
+var sortArr={};
+for (var i in inArr){
+    if (inArr[i].age in sortArr){sortArr[inArr[i].age].push(inArr[i]);
+        } else 
+            sortArr[inArr[i].age]=[inArr[i],];
+    }
+    return sortArr;
+}
+var groupedByAge = group(people);
+console.log(groupedByAge);
